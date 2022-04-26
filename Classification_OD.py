0d9629016a1d7306f2f4d72499379c54a9a6247d
PrimitiveGaming <70715637+PrimitiveGaming@users.noreply.github.com>
2022-04-26 15:41:24 -0500

    Updates made for demo

--- Classification_OD.py
+++ Classification_OD.py
@@ -14,6 +14,7 @@ import timeit
 
 cont_going = True
 capture = pyshark.LiveCapture(output_file="data/capture.pcap")
+iter_count = 0
 while cont_going:
     capture.sniff(timeout=5)
     start = timeit.timeit()
@@ -36,9 +37,14 @@ while cont_going:
                 with open("possible_anomalies.csv", "a") as f:
                     temp_write = str(capture[count].ip)
                     temp_write = temp_write.replace("\n", ",").strip()
+                    print("Anomaly Detected from " + str(capture[count].ip.src) + " to " + str(capture[count].ip.dst))
                     f.write(str(temp_write) + "\n")
             except:
                 print("Could not Access Packet Layer")
         count += 1
     end = timeit.timeit()
-    print("Total Down Time between Intervals: " + str(end - start))
+    print("Total Down Time between Intervals: " + str(abs(end - start)))
+    iter_count += 1
+    # if statement is used to get out of loop for demo purposes
+    if iter_count > 10:
+        cont_going = False
